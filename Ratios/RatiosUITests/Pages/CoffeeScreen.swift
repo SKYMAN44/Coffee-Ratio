5721f80095db4aefe8c0a19abee48fef5e8fb993
skyman <sokdep@yandex.ru>
2022-04-29 21:50:07 +0300

    Add CoffeeScreen UItests

--- Ratios/RatiosUITests/Pages/CoffeeScreen.swift
+++ Ratios/RatiosUITests/Pages/CoffeeScreen.swift
@@ -7,3 +7,132 @@
 //
 
 import Foundation
+import XCTest
+
+final class CoffeeScreen: BaseScreen {
+    private lazy var startButton = app.buttons["Start"]
+    private lazy var pauseButton = app.buttons["Pause"]
+    private lazy var resetButton = app.buttons["Reset"]
+    private lazy var keyBoardReturn = app.buttons["Return"]
+    private lazy var waterRatioLabel = app.staticTexts["1"]
+    private lazy var screenTitle = app.staticTexts["How much coffee?"]
+    private lazy var waterTopLabel = app.staticTexts["You need"]
+    private lazy var waterBottomLabel = app.staticTexts["grams of water"]
+    private lazy var coffeeAmountLabel = app.staticTexts["0.0"]
+    private lazy var timerLabel = app.staticTexts["00:00"]
+    private lazy var ratioTitle = app.staticTexts["What ratio?"]
+    
+    private lazy var grammsTextField = app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 0)
+    private lazy var waterTextField = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 1)
+    
+    // MARK: - Basic Validation
+    @discardableResult
+    func validateUILoading() -> Self {
+        XCTAssertTrue(startButton.exists)
+        XCTAssertTrue(resetButton.exists)
+        XCTAssertTrue(screenTitle.exists)
+        XCTAssertTrue(ratioTitle.exists)
+        XCTAssertTrue(grammsTextField.exists)
+        XCTAssertTrue(waterTextField.exists)
+        XCTAssertTrue(coffeeAmountLabel.exists)
+        XCTAssertTrue(timerLabel.exists)
+        XCTAssertTrue(waterTopLabel.exists)
+        XCTAssertTrue(waterBottomLabel.exists)
+        XCTAssertTrue(waterRatioLabel.exists)
+        
+        XCTAssertFalse(pauseButton.exists)
+        
+        return self
+    }
+    
+    @discardableResult
+    func checkCalculationsResult(_ correctResult: String) -> Self {
+        XCTAssertTrue(app.staticTexts[correctResult].exists)
+        XCTAssertFalse(coffeeAmountLabel.exists)
+        
+        return self
+    }
+    
+    // MARK: - TextField Manipulations
+    @discardableResult
+    func enterGramms(_ gramms: String) -> Self {
+        grammsTextField.tap()
+        grammsTextField.typeText(gramms)
+        keyBoardReturn.tap()
+        
+        return self
+    }
+    
+    @discardableResult
+    func enterWaterRatio(_ amount: String) -> Self {
+        waterTextField.tap()
+        waterTextField.typeText(amount)
+        keyBoardReturn.tap()
+        
+        return self
+    }
+    
+    // MARK: - Timer Manipulation
+    @discardableResult
+    func startTimer() -> Self {
+        startButton.tap()
+        
+        return self
+    }
+    
+    @discardableResult
+    func pauseTimer() -> Self {
+        pauseButton.tap()
+        
+        return self
+    }
+    
+    @discardableResult
+    func resetTimer() -> Self {
+        resetButton.tap()
+        
+        return self
+    }
+    
+    @discardableResult
+    func checkTimerResult(_ time: String) -> Self {
+        let timerLabelSeconds = app.staticTexts[time]
+        let seconds = convertStringToSeconds(time)
+        XCTAssertTrue(timerLabelSeconds.waitForExistence(timeout: seconds))
+        
+        return self
+    }
+    
+    @discardableResult
+    func verifyTimer(_ time: String) -> Self {
+        startButton.tap()
+        
+        XCTAssertFalse(startButton.exists)
+        XCTAssertTrue(pauseButton.exists)
+        
+        pauseButton.tap()
+        
+        XCTAssertFalse(pauseButton.exists)
+        XCTAssertTrue(startButton.exists)
+        
+        startButton.tap()
+        
+        let timerLabelFiveSeconds = app.staticTexts[time]
+        let seconds = convertStringToSeconds(time)
+        XCTAssertTrue(timerLabelFiveSeconds.waitForExistence(timeout: seconds))
+        
+        resetButton.tap()
+        
+        XCTAssertTrue(timerLabel.exists)
+        
+        return self
+    }
+}
+
+// MARK: - Helper function
+extension CoffeeScreen {
+    private func convertStringToSeconds(_ time: String) -> Double {
+        let components = time.split { $0 == ":" }.map { Double(String($0))! }
+        return components[0] * 60 + components[1]
+    }
+}
--- Ratios/RatiosUITests/Tests/CoffeeScreenTest.swift
+++ Ratios/RatiosUITests/Tests/CoffeeScreenTest.swift
@@ -8,35 +8,56 @@
 
 import XCTest
 
-class CoffeeScreenTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+final class CoffeeScreenTest: BaseTest {
+    lazy var coffeeScreen = CoffeeScreen(app)
+    
+    override func setUp() {
+        super.setUp()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testValidateCoffeeScreenItems() {
+        coffeeScreen.validateUILoading()
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testBasicWorkflow() {
+        coffeeScreen
+            .enterGramms("10")
+            .enterWaterRatio("1")
+            .checkCalculationsResult("10.0")
+            .verifyTimer("00:05")
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testLongTimer() {
+        coffeeScreen.verifyTimer("01:01")
+    }
+    
+    func testDecimalNumbers() {
+        coffeeScreen
+            .enterGramms("12.2")
+            .enterWaterRatio("1")
+            .checkCalculationsResult("12.2")
+//            .enterWaterRatio("2.5")
+//            .checkCalculationsResult("30.5")
+//       this will cause test to fail
+    }
+    
+    func testBigNumbers() {
+        coffeeScreen
+            .enterGramms("10")
+            .enterWaterRatio("10")
+            .checkCalculationsResult("100.0")
+    }
+    
+    func testTimer() {
+        coffeeScreen
+            .startTimer()
+            .checkTimerResult("00:03")
+            .resetTimer()
+            .startTimer()
+            .checkTimerResult("00:02")
+            .pauseTimer()
+            .checkTimerResult("00:02")
+            .resetTimer()
+            .checkTimerResult("00:00")
     }
 }
